fa5cb83d73f9aaa78fba2c30ab9d7e9b5f079019
Kevin <kevinkenny92@gmail.com>
2017-08-03 17:39:22 +0000

    made changes, still in UA

--- config/routes.rb
+++ config/routes.rb
@@ -18,12 +18,12 @@ Rails.application.routes.draw do
     resources :categories, except: [:edit, :update, :show, :destroy]    #Categories #
   end
 
-#Creating LOGIN,  LOGOUT, SIGN UP and
+#Creating LOGIN,  LOGOUT, SIGN UP and USERS
   get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
   get '/signup' => 'users#new'
-
+  post '/users' => 'users#create'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
